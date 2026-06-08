70ddc2af1b4a9f3721e58010471fa88f6f0bc4c0
Fátima Alfaro <fatialfaro99@gmail.com>
2026-06-08 17:53:40 -0300

    Agregue cuatro endpoints uno para agregar, uno para filtrar por id, uno para borrar y uno para listar

--- index.js
+++ index.js
@@ -1,10 +1,75 @@
-import express from 'express';
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
 
 const app = express();
 const PORT = 3000; 
 
+app.use(cors());
+app.use(express.json())
+app.use(morgan('dev'))
 
-console.log("Primer mensaje y segundo mensaje")
+const libros = [
+    {
+        id: 1,
+        titulo: "El Principito",
+        autor: "Antoine de Saint-Exupéry"
+    },
+    {
+        id: 2,
+        titulo: "Harry Potter",
+        autor: "J.K. Rowling"
+    },
+    {
+        id: 3,
+        titulo: "Don Quijote de la Mancha",
+        autor: "Miguel de Cervantes"
+    }
+]
+
+app.get('/api/libros', (req, res) => {
+    res.json(libros)
+})
+
+app.get('/api/libros/:id', (req, res) => {
+
+    const id = parseInt(req.params.id)
+
+    const libroEncontrado = libros.find(
+        libro => libro.id === id
+    )
+
+    res.json(libroEncontrado)
+})
+
+
+app.post('/api/libros', (req, res) => {
+
+    const nuevoLibro = req.body
+
+    libros.push(nuevoLibro)
+
+    res.json({
+        mensaje: 'Libro agregado correctamente',
+        libro: nuevoLibro
+    })
+})
+
+app.delete('/api/libros/:id', (req, res) => {
+
+    const id = parseInt(req.params.id)
+
+    const indice = libros.findIndex(
+        libro => libro.id === id
+    )
+
+    libros.splice(indice, 1)
+
+    res.json({
+        mensaje: 'Libro eliminado correctamente'
+    })
+})
+
 
 app.listen(PORT, () => {
     console.log(`Servidor activo en el puerto ${PORT}`)
